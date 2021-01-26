50401bde8d1150382be6f40b69fd20ba8bcdb6a6
Chris Sanders <zidane@spongepowered.org>
2021-01-26 13:36:32 -0600

    Add licenser and apply licensing.
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-gradle-plugin`
     `maven-publish`
+    id("org.cadixdev.licenser") version "0.5.0"
 }
 
 group = "org.spongepowered"
@@ -13,6 +14,16 @@ repositories {
     }
 }
 
+dependencies {
+    implementation("com.google.code.gson:gson:2.8.6")
+    implementation("de.undercouch:gradle-download-task:4.1.1")
+    implementation("net.minecraftforge:mergetool:1.1.1")
+    implementation("org.cadixdev:atlas:0.2.0")
+    implementation("org.cadixdev:lorenz:0.5.6")
+    implementation("org.cadixdev:lorenz-asm:0.5.4")
+    implementation("org.cadixdev:lorenz-io-proguard:0.5.6")
+}
+
 gradlePlugin {
     plugins {
         create("vanillagradle") {
@@ -22,12 +33,18 @@ gradlePlugin {
     }
 }
 
-dependencies {
-    implementation("com.google.code.gson:gson:2.8.6")
-    implementation("de.undercouch:gradle-download-task:4.1.1")
-    implementation("net.minecraftforge:mergetool:1.1.1")
-    implementation("org.cadixdev:atlas:0.2.0")
-    implementation("org.cadixdev:lorenz:0.5.6")
-    implementation("org.cadixdev:lorenz-asm:0.5.4")
-    implementation("org.cadixdev:lorenz-io-proguard:0.5.6")
+val name: String by project
+val organization: String by project
+val projectUrl: String by project
+
+license {
+    (this as ExtensionAware).extra.apply {
+        this["name"] = name
+        this["organization"] = organization
+        this["url"] = projectUrl
+    }
+    header = project.file("HEADER.txt")
+
+    include("**/*.java")
+    newLine = false
 }
