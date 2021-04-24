b6ddeb894c4af87ec9a0798346a873f4fda0d295
zml <zml@stellardrift.ca>
2021-04-23 23:23:26 -0700

    Resolve variable overlap issues for almost all cases

--- src/main/java/org/spongepowered/gradle/vanilla/asm/LocalVariableNamer.java
+++ src/main/java/org/spongepowered/gradle/vanilla/asm/LocalVariableNamer.java
@@ -24,6 +24,7 @@
  */
 package org.spongepowered.gradle.vanilla.asm;
 
+import org.objectweb.asm.Handle;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.spongepowered.gradle.vanilla.Constants;
@@ -31,34 +32,86 @@ import org.spongepowered.gradle.vanilla.Constants;
 public final class LocalVariableNamer extends MethodVisitor {
 
     private final boolean isStatic;
-    private final int paramSlotCount;
+    private final int paramTotal;
     private int parameterCount = 0;
+    private int lvtCount;
+    private final VariableScopeTracker scopeTracker;
+    private final VariableScope scope;
 
-    public LocalVariableNamer(final boolean isStatic, final int paramSlotCount, final MethodVisitor methodVisitor) {
+    public LocalVariableNamer(
+        final boolean isStatic,
+        final int paramTotal,
+        final VariableScopeTracker scopeTracker,
+        final VariableScope scope,
+        final MethodVisitor methodVisitor
+    ) {
         super(Constants.ASM_VERSION, methodVisitor);
         this.isStatic = isStatic;
-        this.paramSlotCount = paramSlotCount;
+        this.paramTotal = paramTotal;
+        this.scopeTracker = scopeTracker;
+        this.scope = scope;
     }
 
     @Override
     public void visitParameter(final String name, final int access) {
         // Normalize parameter attributes
-        super.visitParameter("param" + this.parameterCount++, access);
+        if (LocalVariableNamer.isValidJavaIdentifier(name)) {
+            super.visitParameter(this.scope.produceSafe(name, VariableScope.Usage.PARAMETER), access);
+        } else {
+            super.visitParameter(this.scope.produceSafe("param" + this.parameterCount, VariableScope.Usage.PARAMETER), access);
+        }
+
+        this.parameterCount++;
+    }
+
+    @Override
+    public void visitInvokeDynamicInsn(
+        final String name, final String descriptor, final Handle bootstrapMethodHandle, final Object... bootstrapMethodArguments
+    ) {
+        for (final Object arg : bootstrapMethodArguments) {
+            if (arg instanceof Handle) {
+                final Handle referenced = (Handle) arg;
+                if (referenced.getOwner().equals(this.scopeTracker.className())) { // a reference to a method within ourselves, assume nested scope
+                    this.scopeTracker.makeChild(this.scope, referenced.getName(), referenced.getDesc());
+                }
+            }
+        }
+        super.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments);
     }
 
     @Override
     public void visitLocalVariable(final String name, final String descriptor, final String signature, final Label start, final Label end,
             final int index) {
-        final int paramSlotOffset = this.isStatic ? this.paramSlotCount : this.paramSlotCount + 1;
+        final int varNumber = this.lvtCount++;
+        final int paramLocalVarSplit = this.isStatic ? this.paramTotal : this.paramTotal + 1;
         final String varName;
-        if (index == 0 && !this.isStatic) {
+        if (LocalVariableNamer.isValidJavaIdentifier(name)) {
+            varName = name;
+        } else if (varNumber == 0 && !this.isStatic) {
             varName = "this";
-        } else if (index < paramSlotOffset) {
-            varName = "param" + (index - (this.isStatic ? 0 : 1));
+        } else if (varNumber < paramLocalVarSplit) {
+            varName = "param" + (varNumber - (this.isStatic ? 0 : 1));
         } else {
-            varName = "var" + (index - paramSlotOffset);
+            varName = "var" + (varNumber - paramLocalVarSplit);
         }
 
-        super.visitLocalVariable(varName, descriptor, signature, start, end, index);
+        super.visitLocalVariable(this.scope.produceSafe(varName, VariableScope.Usage.LVT), descriptor, signature, start, end, index);
+    }
+
+    private static boolean isValidJavaIdentifier(final String name) {
+        if (name.isEmpty()) {
+            return false;
+        }
+        if (!Character.isJavaIdentifierStart(name.charAt(0))) {
+            return false;
+        }
+
+        for (int i = 1; i < name.length(); i++) {
+            if (!Character.isJavaIdentifierPart(name.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
