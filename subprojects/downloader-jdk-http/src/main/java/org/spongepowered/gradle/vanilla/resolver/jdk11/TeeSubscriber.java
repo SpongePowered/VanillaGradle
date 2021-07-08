49883e1610d69862b65241ae4dc0d19e7e21e260
zml <zml@stellardrift.ca>
2021-07-08 15:24:41 -0700

    downloader-jdk-http: Rough impl of downloader with J11 HttpClient

---
+++ subprojects/downloader-jdk-http/src/main/java/org/spongepowered/gradle/vanilla/resolver/jdk11/TeeSubscriber.java
@@ -0,0 +1,110 @@
+/*
+ * This file is part of VanillaGradle, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.gradle.vanilla.resolver.jdk11;
+
+import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Flow;
+
+public class TeeSubscriber<V> implements HttpResponse.BodySubscriber<V> {
+    private final HttpResponse.BodySubscriber<V> first;
+    private final HttpResponse.BodySubscriber<?>[] others;
+    private ProxySubscription subscription;
+
+    public TeeSubscriber(final HttpResponse.BodySubscriber<V> first, final HttpResponse.BodySubscriber<?>... others) {
+        this.first = first;
+        this.others = others;
+    }
+
+    @Override
+    public CompletionStage<V> getBody() {
+        return this.first.getBody();
+    }
+
+    @Override
+    public void onSubscribe(final Flow.Subscription subscription) {
+        final var proxySub = new ProxySubscription(subscription);
+        this.subscription = proxySub;
+        this.first.onSubscribe(proxySub);
+        for (final var other : this.others) {
+            other.onSubscribe(proxySub);
+        }
+        proxySub.flush();
+    }
+
+    @Override
+    public void onNext(final List<ByteBuffer> item) {
+        final var proxySub = this.subscription;
+        this.first.onNext(item);
+        for (final var other : this.others) {
+            other.onNext(item);
+        }
+        proxySub.flush();
+    }
+
+    @Override
+    public void onError(final Throwable throwable) {
+        this.first.onError(throwable);
+        for (final var other : this.others) {
+            other.onError(throwable);
+        }
+    }
+
+    @Override
+    public void onComplete() {
+        this.first.onComplete();
+        for (final var other : this.others) {
+            other.onComplete();
+        }
+    }
+
+    static class ProxySubscription implements Flow.Subscription {
+        private final Flow.Subscription original;
+        private long requested;
+
+        ProxySubscription(final Flow.Subscription original) {
+            this.original = original;
+        }
+
+        @Override
+        public void request(final long n) {
+           this.requested = this.requested == 0 ? n : Math.min(this.requested, n);
+        }
+
+        void flush() {
+            if (this.requested > 0) {
+                this.original.request(this.requested);
+                this.requested = 0;
+            }
+        }
+
+        @Override
+        public void cancel() {
+            this.original.cancel();
+        }
+    }
+}
---
+++ subprojects/downloader-jdk-http/src/main/java/org/spongepowered/gradle/vanilla/resolver/jdk11/ValidatingBodySubscriber.java
@@ -0,0 +1,86 @@
+/*
+ * This file is part of VanillaGradle, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.gradle.vanilla.resolver.jdk11;
+
+import org.spongepowered.gradle.vanilla.resolver.HashAlgorithm;
+
+import java.io.IOException;
+import java.net.http.HttpResponse;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Flow;
+
+class ValidatingBodySubscriber<T> implements HttpResponse.BodySubscriber<T> {
+
+    private final HttpResponse.BodySubscriber<T> original;
+    private final HashAlgorithm algorithm;
+    private final MessageDigest md;
+    private final String expectedHash;
+
+    public ValidatingBodySubscriber(final HashAlgorithm algo, final HttpResponse.BodySubscriber<T> original, final String expectedHash) {
+        this.original = original;
+        this.algorithm = algo;
+        this.md = algo.digest();
+        this.expectedHash = expectedHash;
+    }
+
+    @Override
+    public CompletionStage<T> getBody() {
+        return this.original.getBody();
+    }
+
+    @Override
+    public void onSubscribe(final Flow.Subscription subscription) {
+        this.original.onSubscribe(subscription);
+    }
+
+    @Override
+    public void onNext(final List<ByteBuffer> item) {
+        for (int i = 0, size = item.size(); i < size; i++) {
+            final ByteBuffer buf = item.get(i);
+            final int pos = buf.position();
+            this.md.update(buf);
+            buf.position(pos);
+        }
+        this.original.onNext(item);
+    }
+
+    @Override
+    public void onError(final Throwable throwable) {
+        this.original.onError(throwable);
+    }
+
+    @Override
+    public void onComplete() {
+        final String actual = HashAlgorithm.toHexString(this.md.digest());
+        if (!actual.equals(this.expectedHash)) {
+            this.original.onError(new IOException("Failed to validate " + this.algorithm.digestName() + " hash. Expected " + this.expectedHash + ", but got " + actual));
+        } else {
+            this.original.onComplete();
+        }
+    }
+}
