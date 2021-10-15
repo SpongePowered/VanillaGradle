c25bfacd4d7d2a7c354065bfd7cac7d251eaf412
Joe <burtonjae@hotmail.co.uk>
2021-10-15 13:34:22 +0100

    Revert "Remap jar task"
    This reverts commit 2bb3fa918a5157e4b8fe58b3d2e5fc2699f24667.

--- subprojects/gradle-plugin/src/main/java/org/spongepowered/gradle/vanilla/internal/task/JarEntryTransformerProvider.java
+++
@@ -1,9 +0,0 @@
-package org.spongepowered.gradle.vanilla.internal.task;
-
-import org.cadixdev.bombe.jar.JarEntryTransformer;
-
-import java.io.IOException;
-
-public interface JarEntryTransformerProvider {
-    JarEntryTransformer getJarEntryTransformer() throws IOException;
-}
