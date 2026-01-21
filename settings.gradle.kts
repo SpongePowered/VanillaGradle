6d0b14f02a81ab87680aa953c0030d03fae267c2
Yeregorix <yeregorixcraft@gmail.com>
2026-01-21 16:05:37 +0100

    Merge subprojects
    They are no longer needed since we use the JDK downloader

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,19 +15,4 @@ dependencyResolutionManagement {
     pluginManagement.repositories.forEach(repositories::add)
 }
 
-rootProject.name = "vanillagradle-parent"
-
-// The Gradle plugin maintains a different name convention to preserve history
-include("gradle-plugin")
-findProject(":gradle-plugin")?.apply {
-    name = "vanillagradle"
-    projectDir = file("subprojects/gradle-plugin")
-}
-
-sequenceOf("resolver-core", "downloader-jdk-http").forEach {
-    include(it)
-    findProject(":$it")?.apply {
-        name = "vanillagradle-$it"
-        projectDir = file("subprojects/$it")
-    }
-}
+rootProject.name = "vanillagradle"
